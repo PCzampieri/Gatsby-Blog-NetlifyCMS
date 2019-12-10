5a074bf2cbe9e563547cae5bbbd26acd32666262
Paulo Cezar Lima Zampieri <pczampieri@hotmail.com>
2019-12-10 12:06:09 -0300

    first version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,55 @@
 module.exports = {
   siteMetadata: {
     title: `Blog`,
+    position: `FullSTACK DEV`,
     description: `A blog about frontend development and other coll stuff.`,
     author: `@blog`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    // needs to be the first to work gatsby-remark-images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uploads`,
+        path: `${__dirname}/static/assets/img`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 960,
+              linkImagesToOriginal: false,
+            },
+          },
+          `gatsby-remark-lazy-load`,
+          `gatsby-remark-prismjs`,
+        ],
       },
     },
     `gatsby-transformer-sharp`,
@@ -31,4 +70,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/pczampieri",
+  },
+  {
+    label: "Twitter",
+    url: "https://twuitter.com/pczampieri",
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/pczampieri",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/pczampieri",
+  },
+  {
+    label: "Unsplash",
+    url: "https://unsplash.com/pczampieri",
+  },
+];
+
+export default links;
